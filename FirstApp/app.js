141e581a9f1790be3c93f84858d0fcc073fa840b
Owais Khan <kowais915@gmail.com>
2020-08-30 14:55:00 +0500

    A simple Angular app was created. The app.js and index.html were linked together

---
+++ FirstApp/app.js
@@ -0,0 +1,16 @@
+(function(){
+'use strict';
+    
+angular.module('MyFirstApp', [])
+
+    .controller('MyFirstController', function($scope){
+    $scope.name = "Owais";
+    });
+
+
+
+
+
+
+    
+})();
